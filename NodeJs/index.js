8452cc918bd673e29f788daf4cb01dccbbbdf494
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2026-01-02 10:16:43 -0300

    add http.js

--- NodeJs/index.js
+++ NodeJs/index.js
@@ -3,7 +3,9 @@ const { Person } = require("./person");
 
 //require("./modules/path.js");
 
-require("./modules/fs.js");
+//require("./modules/fs.js");
+
+require("./modules/http.js");
 
 //Apenas o nome do arquivo atual
 const person = new Person("Felipe");
