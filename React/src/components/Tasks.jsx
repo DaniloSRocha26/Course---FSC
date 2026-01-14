b3d9d615398d5f5583ec1268052d84f24c96795a
Danilo.Dev <182436555+DaniloSRocha26@users.noreply.github.com>
2026-01-14 12:05:41 -0300

    correção dos props

--- React/src/components/Tasks.jsx
+++ React/src/components/Tasks.jsx
@@ -1,9 +1,11 @@
 function Tasks(props) {
   console.log(props);
   return (
-    <ul>
+    <ul className="space-y-4">
       {props.tasks.map((tasks) => (
-        <li className="bg-slate-400 text-white p-2">{tasks.title}</li>
+        <li className="bg-slate-400 text-white p-2 rounded-md">
+          {tasks.title}
+        </li>
       ))}
     </ul>
   );
