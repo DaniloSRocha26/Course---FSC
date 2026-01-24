adaff2fc52439e2a7ac393a9d4e1691120f91148
Danilo.Dev <Danilo.Santana26@Outlook.com.br>
2026-01-24 04:47:23 -0300

    final commit

--- React/src/components/Test.jsx
+++ React/src/components/Test.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 
 class Test extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: "Hello World",
-    };
-  }
-  render() {
-    return <h1>{this.state.message}</h1>;
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello World",
+        };
+    }
+
+    componentDidMount() {
+        console.log("É executado quando o usuário acessa pela primeira vez");
+    }
+    render() {
+        return <h1>{this.state.message}</h1>;
+    }
 }
 
 export default Test;
